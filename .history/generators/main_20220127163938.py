15f3a33a19263f6b33f86221dbb00f5e5535aa98
TomaszBeben <tbeben@st.swps.edu.pl>
2022-01-28 01:17:29 +0100

    generator (yield)

---
+++ .history/generators/main_20220127163938.py
@@ -0,0 +1,7 @@
+def gen():
+    i = 0
+    while i < 5:
+        yield i
+        i += 1
+
+print(gen())
---
+++ .history/generators/main_20220127164251.py
@@ -0,0 +1,10 @@
+def gen():
+    i = 0
+    while i < 5:
+        yield i
+        i += 1
+
+for i in gen():
+    print(i)
+
+print(gen())
