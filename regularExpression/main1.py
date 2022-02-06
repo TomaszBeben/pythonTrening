f7abd05cb363aa6d93cbc0778df6988564ec52cf
TomaszBeben <tbeben@st.swps.edu.pl>
2022-02-06 21:55:52 +0100

    regular expressions v1

---
+++ regularExpression/main1.py
@@ -0,0 +1,33 @@
+import re
+
+# r- przed 'stringiem' ignoruje wszeystkie znaki specjalne!!!!
+
+pattern = r'word'
+text = r'textwordtexttext'
+
+print(pattern)
+# print(re.match(r'.*' + pattern + r'.*', text))- szuka po całoscie
+print(re.match(r'.*' + pattern + r'.*', text))
+#  match- szuka czy wzor znajduje na poszatku textu
+
+if re.search(pattern, text):
+    print('jest')
+else:
+    print('nie ma')
+# search szuka po calym texcie
+
+if re.findall(pattern, text):
+    print('jest')
+else:
+    print('nie ma')
+# findall wyszukuje wszystkie
+
+matching = re.search(pattern, text)
+if matching:
+    print(matching.group())
+    print(matching.start())
+    print(matching.end())
+    print(matching.span())
+
+text2  = re.sub(pattern, r'something', text)
+print(text2)
